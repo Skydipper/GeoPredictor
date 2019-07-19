7c50097f2d1668d40fcfbe4cd43daa5eb347b24e
Enrique Cornejo <enrique@cornejo.me>
2019-07-19 11:48:33 +0200

    Entrypoint points to main.py

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 case "$1" in
     develop)
         echo "Running Development Server"
-        exec python Validator/app.py
+        exec python Validator/main.py
         ;;
     test)
         echo "Test"
