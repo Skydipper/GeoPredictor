0c59e4256404f5680677d3e0764bec0f0c4c823f
Elena Palao Martinez <elena_palao@MacBook-Pro-de-Elena.local>
2019-07-19 12:09:50 +0200

    add get schema

--- Validator/__init__.py
+++ Validator/__init__.py
@@ -9,8 +9,19 @@ def hello_world(name='world'):
     """A hello world func"""
     return f"Hello {name}"
 
-@app.route('/post',  strict_slashes=False, methods=['POST'])
+@app.route('/schema',  strict_slashes=False, methods=['POST'])
 @parse_payload
 def post_schema(payload):
     logging.info(f'[POST]: Recieved {payload}')
+    # function to post schema
+
+    return jsonify(payload), 200
+
+@app.route('/schema/<id>',  strict_slashes=False, methods=['GET'])
+@parse_payload
+def get_schema(id):
+    #function to get schema id from mongo
+    logging.info(f'[GET]: Recieved {payload}')
+    
+
     return jsonify(payload), 200
