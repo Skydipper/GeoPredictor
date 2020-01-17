329007d9a239e870da44eec63c018c28bcf30c0a
Alicia <alicia.arenzana@gmail.com>
2020-01-17 10:29:34 +0100

    import data to the DB

--- README.md
+++ README.md
@@ -6,10 +6,16 @@ Lists all models and allows filtering by datasetID, type, architecture...
 The knowledge sorce came from this [medium article](https://medium.com/@renato.groffe/postgresql-pgadmin-4-docker-compose-montando-rapidamente-um-ambiente-para-uso-55a2ab230b89)
 
 ## development
-You will need to have installed docker and docker-compose; also you will  need to have control tower and geostore microservices up and running  
+You will need to have installed docker and docker-compose; 
+You will  need to have control tower and geostore microservices up and running.
+
 run `sh geopredictor.sh develop` 
- After pgadmin is up and running you can `http://0.0.0.0:16543/` to load into pg-admin
+ After pgadmin is up and running you can `http://0.0.0.0:16543/` to load into pg-admin.
  for the microservice endpoint you should be able to access `http://0.0.0.0:6868/`
  
- you need to create a new server connection and in network add the docker network name, the port used on postgres and the login info (all available on the env variables)
-
+In order to populate the DB you will need to update the data as you need on the `/data`  folder. 
+You will need to connect to the postgres container. To do so:
+`docker exec -it geo-postgres-compose /bin/bash
+`
+`cd /data_import`
+`sh import_data.sh`
---
+++ data/import_data.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+
+THE_USER=postgres
+THE_DB=geomodels
+PSQL=/opt/postgresql/bin/psql
+THE_DIR=/data_import/
+TABLES="model model_versions image dataset"
+
+
+if psql -U ${THE_USER} -lqt | cut -d \| -f 1 | grep -qw ${THE_DB} 
+then
+    # database exists
+    echo 'NOTICE:  Database '${THE_DB}' exists; Generating tables...'
+else
+    echo 'NOTICE:  Database does not exist; creating ${THE_DB} ...'
+    ### this will help us create the database
+	#${PSQL} -U ${THE_USER}<<OMG
+	#CREATE DATABASE ${THE_DB};
+#OMG
+#
+fi
+
+## this will help us create the database model
+psql -U ${THE_USER} ${THE_DB}<<OMG
+DROP TABLE IF EXISTS model, model_versions, image, dataset;
+OMG
+echo "NOTICE:  finish deleting tables."
+psql -U ${THE_USER} ${THE_DB}<<OMG
+CREATE TABLE model (
+	id INT PRIMARY KEY,
+	uid TEXT,
+	model_name TEXT,
+	model_description TEXT,
+	model_type TEXT,
+	input_image_id TEXT,
+	output_image_id TEXT,
+	n_versions INT,
+	model_architecture TEXT
+);
+CREATE TABLE model_versions (
+	id INT PRIMARY KEY,
+	model_id TEXT,
+	geostore_id TEXT,
+	version INT,
+	training_params JSONB,
+	status INT
+);
+CREATE TABLE image (  
+	id INT PRIMARY KEY,
+	uid TEXT,
+	name TEXT,
+	dataset_id TEXT,
+	band_selection INT[],
+	scale INT,
+	init_date DATE,
+	end_date DATE,
+	composite_method TEXT,
+	status INT,
+	bands_min_max JSONB
+);
+CREATE TABLE dataset (
+	id INT PRIMARY KEY,
+	uid TEXT,
+	dataset_id INT,
+	name TEXT,
+	bands JSONB,
+	rgb_bands JSONB,
+	provider INT
+);
+OMG
+echo "NOTICE:  finish creating tables."
+for NAME in ${TABLES}; do
+	psql -U ${THE_USER} ${THE_DB} <<OMG
+    DELETE FROM ${NAME}; 
+    COPY ${NAME} FROM '${THE_DIR}${NAME}.csv' delimiter ',' CSV header;
+OMG
+done
+
+echo "SUCCESS:  Tables import ready"
