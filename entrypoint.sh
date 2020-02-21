6e43acf003e842d8eac3d34716fef06630858bbf
stasgalin <stas.galin@do-it.co>
2020-02-21 11:52:35 +0200

    added script for start prod in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,10 @@ case "$1" in
         echo "Running Development Server"
         exec python main.py
         ;;
+    start)
+        echo "Running Production Server"
+        exec python main.py
+        ;;
     test)
         echo "Test"
         ;;
