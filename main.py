f169fc3e3b0d5798ff505aaa7a6b602c9c395c3e
stanislawgalin <stas,galin@do-it.co>
2020-03-04 18:35:48 +0200

    fix typo with wrong place for getting PORT from env

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from GeoPredictor import app
 import os
 
+port = os.getenv("PORT")
+
 if __name__ == "__main__":
     app.run(
         host='0.0.0.0',
         debug=True,
-        port=os.getenv("PORT")
+        port=port)
